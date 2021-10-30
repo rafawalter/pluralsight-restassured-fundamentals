809d303b905ce550df67ada6b15e38ac1dca662a
rafawalter <rafawalter@gmail.com>
2021-10-30 18:31:44 -0300

    How to get a GitHub Personal access token

--- src/test/java/com/mypluralsight/restassuredFundamentals/TokenHelper.java
+++ src/test/java/com/mypluralsight/restassuredFundamentals/TokenHelper.java
@@ -3,6 +3,7 @@ package com.mypluralsight.restassuredFundamentals;
 public class TokenHelper {
 
     public static String get() {
+        // https://github.com/settings/tokens
         return "";
     }
 }
