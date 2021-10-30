426fc29871090d95bd2b6147eee5a986b6647409
rafawalter <rafawalter@gmail.com>
2021-10-30 18:38:09 -0300

    Custom http verbs

---
+++ src/test/java/com/mypluralsight/restassuredFundamentals/_72CustomRequest.java
@@ -0,0 +1,33 @@
+package com.mypluralsight.restassuredFundamentals;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import org.testng.annotations.Test;
+
+import static org.hamcrest.Matchers.containsString;
+
+public class _72CustomRequest {
+
+    public static final String BASE_URL = "https://api.github.com/";
+
+    @Test
+    public void customRequest() {
+        // both equivalent to RestAssured.get(...)
+        RestAssured.request(Method.GET, BASE_URL)
+                .then()
+                .statusCode(200)
+                .body(containsString("current_user_url"));
+
+        RestAssured.request("GET", BASE_URL)
+                .then()
+                .statusCode(200)
+                .body(containsString("current_user_url"));
+    }
+
+    @Test
+    public void traceExample() {
+        RestAssured.request(Method.TRACE, BASE_URL)
+                .then()
+                .statusCode(405); // Method not allowed
+    }
+}
