9c98194214d6ce67ba4169853c7c3f964503a345
rafawalter <rafawalter@gmail.com>
2021-10-30 18:30:21 -0300

    POST, PATH and DELETE

---
+++ src/test/java/com/mypluralsight/restassuredFundamentals/TokenHelper.java
@@ -0,0 +1,8 @@
+package com.mypluralsight.restassuredFundamentals;
+
+public class TokenHelper {
+
+    public static String get() {
+        return "";
+    }
+}
--- src/test/java/com/mypluralsight/restassuredFundamentals/_71OtherMethods.java
+++ src/test/java/com/mypluralsight/restassuredFundamentals/_71OtherMethods.java
@@ -6,10 +6,10 @@ import org.testng.annotations.Test;
 public class _71OtherMethods {
 
     public static final String BASE_URL = "https://api.github.com/user/repos";
-    public static final String TOKEN = "";
+    public static final String TOKEN = TokenHelper.get();
 
     @Test(description = "Create a repo")
-    public void optionsTest() {
+    public void postTest() {
 
         RestAssured
                 .given()
@@ -20,4 +20,36 @@ public class _71OtherMethods {
                 .then()
                 .statusCode(201);
     }
+
+    @Test(
+            description = "Update a repo",
+            dependsOnMethods = "postTest"
+    )
+    public void patchTest() {
+
+        RestAssured
+                .given()
+                .header("Authorization", "token " + TOKEN)
+                .body("{\"name\": \"deleteme-patched\"}")
+                .when()
+                .patch("https://api.github.com/repos/rafawalter/deleteme")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test(
+            description = "Delete a repo",
+            dependsOnMethods = "patchTest"
+    )
+    public void deleteTest() {
+
+        RestAssured
+                .given()
+                .header("Authorization", "token " + TOKEN)
+                .when()
+                .delete("https://api.github.com/repos/rafawalter/deleteme-patched")
+                .then()
+                .statusCode(204);
+    }
+
 }
